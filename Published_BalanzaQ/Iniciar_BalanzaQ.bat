197740ae32a36fb0d4da344eb767e40d7d6c5466
josfabgom <josfabgom@users.noreply.github.com>
2026-04-12 06:46:58 -0300

    Full workspace cleanup and update to v3.5.45 (Final Stable)

--- Published_BalanzaQ/Iniciar_BalanzaQ.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Iniciando Sistema BalanzaQ Portable...
-start "" "BalanzaQ.Web.exe" --urls "http://0.0.0.0:5069"
-timeout /t 5
-start http://localhost:5069
-echo Sistema iniciado en http://localhost:5069
-pause
